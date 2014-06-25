f574d05049025ad37c56932a69b6a01d29d1c185
Michael Carver <michael.carver@fieldnation.com>
2014-06-25 11:42:12 -0500

    Stubbed out ANDR-14. More refactoring.

---
+++ projects/FieldNationPhase1/src/com/fieldnation/MessageView.java
@@ -0,0 +1,37 @@
+package com.fieldnation;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MessageView extends RelativeLayout {
+	private View _statusView;
+	private TextView _titleTextView;
+	private TextView _substatusTextView;
+	private TextView _messageBodyTextView;
+	private TextView _timeTextView;
+
+	/*-*****************************-*/
+	/*-			LifeCycle			-*/
+	/*-*****************************-*/
+	public MessageView(Context context) {
+		this(context, null, -1);
+	}
+
+	public MessageView(Context context, AttributeSet attrs) {
+		this(context, attrs, -1);
+	}
+
+	public MessageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		final LayoutInflater inflater = LayoutInflater.from(getContext());
+		inflater.inflate(R.layout.view_message, this);
+
+		if (isInEditMode())
+			return;
+	}
+
+}
