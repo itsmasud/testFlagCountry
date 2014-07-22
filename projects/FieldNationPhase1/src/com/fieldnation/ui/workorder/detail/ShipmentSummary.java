a24440f3813749bf26b042d6b65085454608522b
Michael Carver <michael.carver@fieldnation.com>
2014-07-22 15:18:17 -0500

    Starting work on ANDR-80

--- projects/FieldNationPhase1/src/com/fieldnation/ui/workorder/detail/ShipmentSummary.java
+++ projects/FieldNationPhase1/src/com/fieldnation/ui/workorder/detail/ShipmentSummary.java
@@ -4,11 +4,28 @@ import com.fieldnation.R;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ShipmentSummary extends RelativeLayout {
+	private static final String TAG = "ui.workorder.detail.ShipmentSummary";
 
+	// UI
+	private TextView _trackingIdTextView;
+	private TextView _carrierTextView;
+	private TextView _descTextView;
+	private TextView _directionTextView;
+	private ImageButton _deleteImageButton;
+
+	// Data
+
+	/*-*************************************-*/
+	/*-				Life Cycle				-*/
+	/*-*************************************-*/
 	public ShipmentSummary(Context context) {
 		this(context, null, -1);
 	}
@@ -23,6 +40,24 @@ public class ShipmentSummary extends RelativeLayout {
 
 		if (isInEditMode())
 			return;
+
+		_trackingIdTextView = (TextView) findViewById(R.id.trackingid_textview);
+		_carrierTextView = (TextView) findViewById(R.id.carrier_textview);
+		_descTextView = (TextView) findViewById(R.id.description_textview);
+		_directionTextView = (TextView) findViewById(R.id.direction_textview);
+		_deleteImageButton = (ImageButton) findViewById(R.id.delete_imagebutton);
+		_deleteImageButton.setOnClickListener(_delete_onClick);
 	}
 
+	/*-*********************************-*/
+	/*-				Events				-*/
+	/*-*********************************-*/
+	private View.OnClickListener _delete_onClick = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Method Stub: onClick()
+			Log.v(TAG, "Method Stub: onClick()");
+
+		}
+	};
 }
--- projects/FieldNationPhase1/src/com/fieldnation/ui/workorder/detail/ShipmentView.java
+++ projects/FieldNationPhase1/src/com/fieldnation/ui/workorder/detail/ShipmentView.java
@@ -5,13 +5,25 @@ import com.fieldnation.data.workorder.Workorder;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 public class ShipmentView extends LinearLayout implements WorkorderRenderer {
 	private static final String TAG = "ui.workorder.detail.ShipmentView";
 
 	// UI
+	private EditText _trackingIdEditText;
+	private Button _carrierButton;
+	private EditText _carrierEditText;
+	private EditText _descEditText;
+	private RadioButton _shipToSiteRadio;
+	private Button _addButton;
+	private LinearLayout _shipmentsLayout;
 
 	// Data
 	private Workorder _workorder;
@@ -31,7 +43,40 @@ public class ShipmentView extends LinearLayout implements WorkorderRenderer {
 		if (isInEditMode())
 			return;
 
+		_trackingIdEditText = (EditText) findViewById(R.id.trackingid_edittext);
+
+		_carrierButton = (Button) findViewById(R.id.carrier_button);
+		_carrierButton.setOnClickListener(_carrierButton_onClick);
+
+		_carrierEditText = (EditText) findViewById(R.id.carrier_edittext);
+		_descEditText = (EditText) findViewById(R.id.description_edittext);
+		_shipToSiteRadio = (RadioButton) findViewById(R.id.shiptosite_radio);
+
+		_addButton = (Button) findViewById(R.id.add_button);
+		_addButton.setOnClickListener(_addButton_onClick);
+
+		_shipmentsLayout = (LinearLayout) findViewById(R.id.shipments_linearlayout);
+
 	}
+
+	/*-*********************************-*/
+	/*-				Events				-*/
+	/*-*********************************-*/
+	private View.OnClickListener _carrierButton_onClick = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Method Stub: onClick()
+			Log.v(TAG, "Method Stub: onClick()");
+		}
+	};
+
+	private View.OnClickListener _addButton_onClick = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Method Stub: onClick()
+			Log.v(TAG, "Method Stub: onClick()");
+		}
+	};
 
 	/*-*************************************-*/
 	/*-				Mutators				-*/
