8dd5a4daec0463acc2bf7a0d453f4df3641926a1
Michael Carver <michael.carver@fieldnation.com>
2014-10-21 11:14:58 -0500

    Removing eclipse based files.

--- projects/FieldNationPhase1/src/com/fieldnation/ui/workorder/detail/WorkorderRenderer.java
+++
@@ -1,7 +0,0 @@
-package com.fieldnation.ui.workorder.detail;
-
-import com.fieldnation.data.workorder.Workorder;
-
-public interface WorkorderRenderer {
-	public void setWorkorder(Workorder workorder);
-}
