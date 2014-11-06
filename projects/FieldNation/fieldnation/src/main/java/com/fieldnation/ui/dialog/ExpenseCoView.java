94fa6fc7aa3ab03748471f6623597909f29abd74
Michael Carver <michael.carver@fieldnation.com>
2014-11-06 16:30:40 -0600

    twerking the UI a bit more.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/dialog/ExpenseCoView.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/dialog/ExpenseCoView.java
@@ -3,7 +3,10 @@ package com.fieldnation.ui.dialog;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.fieldnation.R;
 
@@ -11,6 +14,11 @@ import com.fieldnation.R;
  * Created by michael.carver on 11/5/2014.
  */
 public class ExpenseCoView extends RelativeLayout {
+    //Ui
+    private TextView _noExpensesTextView;
+    private LinearLayout _expensesList;
+    private Button _addButton;
+
     public ExpenseCoView(Context context) {
         super(context);
         init();
@@ -32,6 +40,7 @@ public class ExpenseCoView extends RelativeLayout {
         if (isInEditMode())
             return;
 
+        _addButton = (Button)findViewById(R.id.add_button);
     }
 
 }
