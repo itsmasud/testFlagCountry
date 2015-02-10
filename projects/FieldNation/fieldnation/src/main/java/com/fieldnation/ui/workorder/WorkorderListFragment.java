6a48ef21b56c38e7f697e0540853d684b71f51ad
Michael Carver <michael.carver@fieldnation.com>
2015-02-10 13:03:38 -0600

    Fixed a few things I forgot to handle.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/WorkorderListFragment.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/WorkorderListFragment.java
@@ -162,10 +162,6 @@ public class WorkorderListFragment extends Fragment {
         _locationDialog = LocationDialog.getInstance(getFragmentManager(), TAG);
 
         _locationLoadingDialog = OneButtonDialog.getInstance(getFragmentManager(), TAG);
-        _locationLoadingDialog.setData(getString(R.string.dialog_location_loading_title),
-                getString(R.string.dialog_location_loading_body),
-                getString(R.string.dialog_location_loading_button),
-                _locationLoadingDialog_listener);
 
         Log.v(TAG, "Display Type: " + _displayView.getCall());
 
@@ -225,6 +221,11 @@ public class WorkorderListFragment extends Fragment {
         AuthTopicService.subscribeAuthState(getActivity(), 0, TAG, _topicReceiver);
         GaTopic.dispatchScreenView(getActivity(), getGaLabel());
         _gpsLocationService = new GpsLocationService(getActivity());
+
+        _locationLoadingDialog.setData(getString(R.string.dialog_location_loading_title),
+                getString(R.string.dialog_location_loading_body),
+                getString(R.string.dialog_location_loading_button),
+                _locationLoadingDialog_listener);
     }
 
     @Override
