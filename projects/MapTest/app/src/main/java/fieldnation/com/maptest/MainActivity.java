570fb29d58a642bbfb11b257b281035d8e6e8dc4
Michael Carver <michael.carver@fieldnation.com>
2015-03-25 16:21:55 -0500

    added map test project

---
+++ projects/MapTest/app/src/main/java/fieldnation/com/maptest/MainActivity.java
@@ -0,0 +1,90 @@
+package fieldnation.com.maptest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private static final String[] PLACES = {"Paris, France", "Moscow, Russia", "Cairo, Egypt", "Hong Kong, China", "Washington, USA"};
+
+    private ListView _list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _list = (ListView) findViewById(R.id.list_view);
+        _list.setAdapter(_listAdapter);
+        _list.setOnItemClickListener(_list_itemClicked);
+    }
+
+    private final BaseAdapter _listAdapter = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return PLACES.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return PLACES[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ListItemView v = null;
+            if (convertView != null && convertView instanceof ListItemView) {
+                v = (ListItemView) convertView;
+            } else {
+                v = new ListItemView(parent.getContext());
+            }
+
+            v.setText(PLACES[position]);
+            return v;
+        }
+    };
+
+    private final AdapterView.OnItemClickListener _list_itemClicked = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + PLACES[position]));
+            MainActivity.this.startActivity(intent);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
