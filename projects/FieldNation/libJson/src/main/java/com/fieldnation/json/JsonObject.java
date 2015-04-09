2d149386884a380997db50d135e08229d7a759b0
Michael Carver <michael.j.carver@gmail.com>
2015-04-09 15:07:22 -0500

    fixed possible npe by .toString on null

--- projects/FieldNation/libJson/src/main/java/com/fieldnation/json/JsonObject.java
+++ projects/FieldNation/libJson/src/main/java/com/fieldnation/json/JsonObject.java
@@ -184,7 +184,7 @@ public class JsonObject {
     }
 
     public String getString(String path) throws ParseException {
-        return get(path).toString();
+        return get(path) + "";
     }
 
     public int getInt(String path) throws ParseException {
