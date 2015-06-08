70aac4acc7487d9cf8ec5ecf55f4ce0a284e964f
Michael Carver <michael.carver@fieldnation.com>
2015-06-08 16:35:37 -0500

    starting to build up a few of the new UI elements

---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/BottomSheetView.java
@@ -0,0 +1,7 @@
+package com.fieldnation.ui;
+
+/**
+ * Created by Michael Carver on 6/8/2015.
+ */
+public class BottomSheetView {
+}
--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/RightDrawerView.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/RightDrawerView.java
@@ -3,14 +3,21 @@ package com.fieldnation.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.fieldnation.R;
 
 /**
  * Created by Michael Carver on 5/20/2015.
  */
-public class RightDrawerView extends RelativeLayout {
+public class RightDrawerView extends FrameLayout {
+    private static final String TAG = "RightDrawerView";
+
+    // Ui
+    private FrameLayout _container;
+
     public RightDrawerView(Context context) {
         super(context);
         init();
@@ -28,5 +35,16 @@ public class RightDrawerView extends RelativeLayout {
 
     private void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.view_right_drawer, this, true);
+
+        if (isInEditMode())
+            return;
+
+        _container = (FrameLayout) findViewById(R.id.container);
+    }
+
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        _container.addView(child, index, params);
     }
 }
