3ae7e90d206cf7e724c5057b218103ed3d5121a7
SHOAIB AHMED <its.masud@yahoo.com>
2015-07-27 09:07:08 +0600

    Revert "ANDR-649"
    This reverts commit c4d8e4a759794f1d55c04add2bede167cc0713d3.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/dialog/ReasonCoView.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/dialog/ReasonCoView.java
@@ -158,7 +158,6 @@ public class ReasonCoView extends RelativeLayout {
             _expires = _expiresCheckBox.isChecked();
 
             if (_expires && _listener != null)
-                _expiresCheckBox.setChecked(false);
                 _datePicker.show(_fm, TAG);
         }
     };
