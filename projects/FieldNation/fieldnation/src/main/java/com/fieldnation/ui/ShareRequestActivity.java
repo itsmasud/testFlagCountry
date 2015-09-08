d55a7414bb409d74de0518d5b8ca0d86e8ca6934
SHOAIB AHMED <its.masud@yahoo.com>
2015-09-08 17:59:08 +0600

    ANDR-516
    Commit-1: Just to get the feedback from Michael whether I am at the
    right way or not.

---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/ShareRequestActivity.java
@@ -0,0 +1,143 @@
+package com.fieldnation.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.fieldnation.GlobalTopicClient;
+import com.fieldnation.Log;
+import com.fieldnation.R;
+import com.fieldnation.data.profile.Profile;
+import com.fieldnation.service.auth.AuthTopicClient;
+import com.fieldnation.service.auth.AuthTopicService;
+import com.fieldnation.service.auth.OAuth;
+import com.fieldnation.ui.workorder.MyWorkActivity;
+
+/**
+ * Created by shoaib.ahmed on Sept/08/2015.
+ */
+public class ShareRequestActivity extends AuthFragmentActivity {
+    private static final String TAG = "ShareRequestActivity";
+
+    private static final String STATE_PROFILE = "STATE_PROFILE";
+    private static final String STATE_IS_AUTH = "STATE_IS_AUTH";
+    private static final String STATE_SHOWING_DIALOG = "STATE_SHOWING_DIALOG";
+
+    private Profile _profile = null;
+    private boolean _isAuth = false;
+    private boolean _calledMyWork = false;
+    private GlobalTopicClient _globalClient;
+    private AuthTopicClient _authClient;
+
+    public ShareRequestActivity() {
+        super();
+        Log.v(TAG, "Construct");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_uploading_documents_list);
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(STATE_IS_AUTH)) {
+                _isAuth = savedInstanceState.getBoolean(STATE_IS_AUTH);
+            }
+            if (savedInstanceState.containsKey(STATE_PROFILE)) {
+                _profile = savedInstanceState.getParcelable(STATE_PROFILE);
+            }
+        }
+
+        Log.v(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(STATE_IS_AUTH, _isAuth);
+        if (_profile != null) {
+            outState.putParcelable(STATE_PROFILE, _profile);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.v(TAG, "onResume");
+        super.onResume();
+        startService(new Intent(this, AuthTopicService.class));
+        _globalClient = new GlobalTopicClient(_globalTopic_listener);
+        _globalClient.connect(this);
+        _authClient = new AuthTopicClient(_authTopic_listener);
+        _authClient.connect(this);
+
+        AuthTopicClient.dispatchRequestCommand(this);
+    }
+
+    @Override
+    protected void onPause() {
+        _globalClient.disconnect(this);
+        _authClient.disconnect(this);
+        super.onPause();
+    }
+
+
+    private final GlobalTopicClient.Listener _globalTopic_listener = new GlobalTopicClient.Listener() {
+        @Override
+        public void onConnected() {
+            Log.v(TAG, "_globalTopic_listener.onConnected");
+            _globalClient.registerGotProfile();
+        }
+
+        @Override
+        public void onGotProfile(Profile profile) {
+            Log.v(TAG, "_globalTopic_listener.onGotProfile");
+            if (profile != null)
+                Log.v(TAG, profile.toJson().display());
+            _profile = profile;
+            doNextStep();
+        }
+    };
+
+    private final AuthTopicClient.Listener _authTopic_listener = new AuthTopicClient.Listener() {
+        @Override
+        public void onConnected() {
+            _authClient.registerAuthState();
+        }
+
+        @Override
+        public void onAuthenticated(OAuth oauth) {
+            _isAuth = true;
+            doNextStep();
+        }
+
+        @Override
+        public void onNotAuthenticated() {
+            AuthTopicClient.dispatchRequestCommand(ShareRequestActivity.this);
+        }
+    };
+
+    private void doNextStep() {
+        if (!_isAuth)
+            return;
+
+        if (_profile == null)
+            return;
+
+        Log.v(TAG, "doNextStep");
+
+        if (_profile.isProvider()) {
+            if (!_calledMyWork) {
+                _calledMyWork = true;
+                MyWorkActivity.startNew(this);
+                finish();
+            }
+        }
+
+    }
+
+    public static void startNew(Context context) {
+        Intent intent = new Intent(context, ShareRequestActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+    }
+}
---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/dialog/SendToDialog.java
@@ -0,0 +1,281 @@
+package com.fieldnation.ui.dialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.FragmentManager;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fieldnation.App;
+import com.fieldnation.R;
+import com.fieldnation.service.toast.ToastClient;
+import com.fieldnation.utils.misc;
+
+/**
+ * Created by shoaib.ahmed on Sept/08/2015.
+ */
+
+public class SendToDialog extends DialogFragmentBase {
+    private static final String TAG = "SendToDialog";
+
+    // State
+    private static final String STATE_TASKID = "STATE_TASKID";
+    private static final String STATE_TITLE = "STATE_TITLE";
+
+    // UI
+    private TextView _titleTextView;
+    private EditText _trackingIdEditText;
+    private Spinner _carrierSpinner;
+    private EditText _carrierEditText;
+    private TextInputLayout _carrierLayout;
+    private EditText _descriptionEditText;
+    private Spinner _directionSpinner;
+    private Button _okButton;
+    private Button _cancelButton;
+
+    // Data
+    private Listener _listener;
+    private long _taskId = 0;
+    private String _title;
+    private boolean _clear = false;
+    private ArrayAdapter<CharSequence> _carrierAdapter;
+    private ArrayAdapter<CharSequence> _directionAdapter;
+
+    /*-*************************************-*/
+    /*-				Life Cycle				-*/
+    /*-*************************************-*/
+    public static SendToDialog getInstance(FragmentManager fm, String tag) {
+        return getInstance(fm, tag, SendToDialog.class);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(STATE_TASKID))
+                _taskId = savedInstanceState.getLong(STATE_TASKID);
+
+            if (savedInstanceState.containsKey(STATE_TITLE))
+                _title = savedInstanceState.getString(STATE_TITLE);
+        }
+        super.onCreate(savedInstanceState);
+
+        setStyle(STYLE_NO_TITLE, 0);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (_title != null)
+            outState.putString(STATE_TITLE, _title);
+
+        if (_taskId != 0)
+            outState.putLong(STATE_TASKID, _taskId);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.dialog_add_shipment, container, false);
+
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        _titleTextView = (TextView) v.findViewById(R.id.title_textview);
+
+        _trackingIdEditText = (EditText) v.findViewById(R.id.trackingid_edittext);
+        _trackingIdEditText.setOnEditorActionListener(_onEditor);
+
+        _carrierSpinner = (Spinner) v.findViewById(R.id.carrier_spinner);
+        _carrierSpinner.setOnItemSelectedListener(_carrier_selected);
+
+        _carrierAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.carrier_list,
+                R.layout.view_spinner_item);
+        _carrierAdapter.setDropDownViewResource(
+                android.support.design.R.layout.support_simple_spinner_dropdown_item);
+        _carrierSpinner.setAdapter(_carrierAdapter);
+
+        _carrierEditText = (EditText) v.findViewById(R.id.carrier_edittext);
+        _carrierEditText.setOnEditorActionListener(_onEditor);
+
+        _carrierLayout = (TextInputLayout) v.findViewById(R.id.carrier_layout);
+
+        _descriptionEditText = (EditText) v.findViewById(R.id.description_edittext);
+        _descriptionEditText.setOnEditorActionListener(_onEditor);
+
+        _directionSpinner = (Spinner) v.findViewById(R.id.direction_spinner);
+
+        _directionAdapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.direction_list,
+                R.layout.view_spinner_item);
+        _directionAdapter.setDropDownViewResource(
+                android.support.design.R.layout.support_simple_spinner_dropdown_item);
+        _directionSpinner.setAdapter(_directionAdapter);
+
+
+        _okButton = (Button) v.findViewById(R.id.ok_button);
+        _okButton.setOnClickListener(_okButton_onClick);
+
+        _cancelButton = (Button) v.findViewById(R.id.cancel_button);
+        _cancelButton.setOnClickListener(_cancel_onClick);
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (_title != null)
+            _titleTextView.setText(_title);
+
+        if (_clear) {
+            _clear = false;
+            _carrierEditText.setText("");
+            _descriptionEditText.setText("");
+            _trackingIdEditText.setText("");
+        }
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+    }
+
+    public void setListener(Listener listener) {
+        _listener = listener;
+    }
+
+    public void show(CharSequence title, long taskId) {
+        _title = (String) title;
+        _taskId = taskId;
+        _clear = true;
+        show();
+    }
+
+    /*-*********************************-*/
+    /*-				Events				-*/
+    /*-*********************************-*/
+    private final TextView.OnEditorActionListener _onEditor = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            boolean handled = false;
+
+            if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                if (v == _trackingIdEditText) {
+                    if (_carrierLayout.getVisibility() == View.VISIBLE) {
+                        _carrierEditText.requestFocus();
+                        handled = true;
+                    } else {
+                        _descriptionEditText.requestFocus();
+                        handled = true;
+                    }
+                } else if (v == _carrierEditText) {
+                    _descriptionEditText.requestFocus();
+                    handled = true;
+                }
+            }
+
+            return handled;
+        }
+    };
+
+    private final AdapterView.OnItemSelectedListener _carrier_selected = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            if ("Other".equals(_carrierSpinner.getSelectedItem().toString())) {
+                _carrierLayout.setVisibility(View.VISIBLE);
+            } else {
+                _carrierLayout.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+    };
+
+    private final View.OnClickListener _okButton_onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (misc.isEmptyOrNull(_trackingIdEditText.getText().toString())) {
+                ToastClient.toast(App.get(), "Missing tracking number", Toast.LENGTH_SHORT);
+                return;
+            }
+
+            if (misc.isEmptyOrNull(_descriptionEditText.getText().toString())) {
+                ToastClient.toast(App.get(), "Missing description", Toast.LENGTH_SHORT);
+                return;
+            }
+
+            if (_listener != null) {
+                if (_taskId != 0) {
+                    if ("Other".equals(_carrierSpinner.getSelectedItem().toString())) {
+                        _listener.onOk(
+                                _trackingIdEditText.getText().toString(),
+                                "Other",
+                                _carrierEditText.getText().toString(),
+                                _descriptionEditText.getText().toString(),
+                                _directionSpinner.getSelectedItemPosition() == 0,
+                                _taskId);
+                    } else {
+                        _listener.onOk(
+                                _trackingIdEditText.getText().toString(),
+                                _carrierSpinner.getSelectedItem().toString(),
+                                null,
+                                _descriptionEditText.getText().toString(),
+                                _directionSpinner.getSelectedItemPosition() == 0,
+                                _taskId);
+                    }
+
+                } else {
+                    if ("Other".equals(_carrierSpinner.getSelectedItem().toString())) {
+                        _listener.onOk(
+                                _trackingIdEditText.getText().toString(),
+                                "Other",
+                                _carrierEditText.getText().toString(),
+                                _descriptionEditText.getText().toString(),
+                                _directionSpinner.getSelectedItemPosition() == 0);
+                    } else {
+                        _listener.onOk(
+                                _trackingIdEditText.getText().toString(),
+                                _carrierSpinner.getSelectedItem().toString(),
+                                null,
+                                _descriptionEditText.getText().toString(),
+                                _directionSpinner.getSelectedItemPosition() == 0);
+                    }
+                }
+            }
+            SendToDialog.this.dismiss();
+        }
+    };
+
+    private final View.OnClickListener _cancel_onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dismiss();
+        }
+    };
+
+    public interface Listener {
+        void onOk(String trackingId, String carrier, String carrierName, String description, boolean shipToSite, long taskId);
+
+        void onOk(String trackingId, String carrier, String carrierName, String description, boolean shipToSite);
+
+        void onCancel();
+    }
+
+
+}
