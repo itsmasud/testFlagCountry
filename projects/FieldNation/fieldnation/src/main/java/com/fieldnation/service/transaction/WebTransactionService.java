631712a817d126597d4ded6e258457ae98b257ae
Michael Carver <michael.carver@fieldnation.com>
2015-11-02 09:08:18 -0600

    Re-enabled some of the logs. left the bad one.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/transaction/WebTransactionService.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/transaction/WebTransactionService.java
@@ -255,13 +255,13 @@ public class WebTransactionService extends MSService implements WebTransactionCo
 
             // if failed, then exit
             if (trans == null) {
-                // Log.v(TAG, "skip no transaction");
+                Log.v(TAG, "skip no transaction");
                 return false;
             }
 
             // debug if have key, output
             if (!misc.isEmptyOrNull(trans.getKey())) {
-//                Log.v(TAG, "Key: " + trans.getKey());
+                Log.v(TAG, "Key: " + trans.getKey());
             }
 
             // at some point call the web service
@@ -273,7 +273,7 @@ public class WebTransactionService extends MSService implements WebTransactionCo
                 if (trans.useAuth()) {
                     OAuth auth = getAuth();
                     if (!_isAuthenticated) {
-                        // Log.v(TAG, "skip no auth");
+                        Log.v(TAG, "skip no auth");
                         trans.requeue(context);
                         return false;
                     }
@@ -312,15 +312,15 @@ public class WebTransactionService extends MSService implements WebTransactionCo
                 result = HttpJson.run(context, request);
 
                 // debug output
-//                try {
-//                    Log.v(TAG, "ResponseCode: " + result.getResponseCode());
-//                    Log.v(TAG, "ResponseMessage: " + result.getResponseMessage());
-//                    if (!result.isFile() && result.getString() != null) {
-//                        Log.v(TAG, "Result: " + result.getString());
-//                    }
-//                } catch (Exception ex) {
-//                    Log.v(TAG, ex);
-//                }
+                try {
+                    Log.v(TAG, "ResponseCode: " + result.getResponseCode());
+                    Log.v(TAG, "ResponseMessage: " + result.getResponseMessage());
+                    //if (!result.isFile() && result.getString() != null) {
+                    //Log.v(TAG, "Result: " + result.getString());
+                    //}
+                } catch (Exception ex) {
+                    Log.v(TAG, ex);
+                }
 
                 // check for invalid auth
                 if (!result.isFile()
