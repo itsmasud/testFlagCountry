673cfefbc2d4c808395b8863f82e72c7b57526a7
Michael Carver <michael.carver@fieldnation.com>
2015-11-12 11:09:49 -0600

    Added null/empty checks.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/crawler/WebCrawlerService.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/crawler/WebCrawlerService.java
@@ -316,9 +316,15 @@ public class WebCrawlerService extends Service {
 
             for (int i = 0; i < list.size(); i++) {
                 Message message = list.get(i);
-                incRequestCounter(2);
-                PhotoClient.get(WebCrawlerService.this, message.getFromUser().getPhotoUrl(), true, true);
-                PhotoClient.get(WebCrawlerService.this, message.getFromUser().getPhotoThumbUrl(), true, true);
+                if (message.getFromUser() != null) {
+                    if (!misc.isEmptyOrNull(message.getFromUser().getPhotoUrl())) {
+                        incRequestCounter(1);
+                        PhotoClient.get(WebCrawlerService.this, message.getFromUser().getPhotoUrl(), true, true);
+                    } else (!misc.isEmptyOrNull(message.getFromUser().getPhotoThumbUrl())) {
+                        incRequestCounter(1);
+                        PhotoClient.get(WebCrawlerService.this, message.getFromUser().getPhotoThumbUrl(), true, true);
+                    }
+                }
             }
         }
 
