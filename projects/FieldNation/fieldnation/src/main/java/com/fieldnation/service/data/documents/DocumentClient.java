f988fddbc3b92dec02ea751a6e742a94c813c801
Michael Carver <michael.carver@fieldnation.com>
2015-12-02 16:18:47 -0600

    built in checks for stuff we can't handle. Toss them to the OS for handling.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/data/documents/DocumentClient.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/data/documents/DocumentClient.java
@@ -1,12 +1,19 @@
 package com.fieldnation.service.data.documents;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.widget.Toast;
 
+import com.fieldnation.App;
+import com.fieldnation.FileHelper;
 import com.fieldnation.Log;
 import com.fieldnation.UniqueTag;
+import com.fieldnation.service.toast.ToastClient;
 import com.fieldnation.service.topics.TopicClient;
 
 import java.io.File;
@@ -27,6 +34,21 @@ public class DocumentClient extends TopicClient implements DocumentConstants {
     }
 
     public static void downloadDocument(Context context, long documentId, String url, String filename, boolean isSync) {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            } catch (Exception ex) {
+                Log.v(STAG, ex);
+
+                ToastClient.toast(context, "URL copied to clipboard. Could not download or view " + url, Toast.LENGTH_LONG);
+
+                ClipboardManager clip = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                clip.setPrimaryClip(ClipData.newRawUri("file download", Uri.parse(url)));
+            }
+            return;
+        }
         Intent intent = new Intent(context, DocumentService.class);
         intent.putExtra(PARAM_ACTION, PARAM_ACTION_DOWNLOAD_DOCUMENT);
         intent.putExtra(PARAM_DOCUMENT_ID, documentId);
