e87244e961f93fb70a66109a633330c6b0561de3
Michael Carver <michael.j.carver@gmail.com>
2016-08-19 13:27:56 -0500

    working out the interactions

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/nav/NavActivity.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/nav/NavActivity.java
@@ -3,19 +3,28 @@ package com.fieldnation.ui.nav;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.fieldnation.R;
+import com.fieldnation.data.profile.Profile;
 import com.fieldnation.fnlog.Log;
-import com.fieldnation.ui.AuthActionBarActivity;
+import com.fieldnation.ui.AuthSimpleActivity;
 
 /**
  * Created by Michael on 8/19/2016.
  */
-public class NavActivity extends AuthActionBarActivity {
+public class NavActivity extends AuthSimpleActivity {
     private static final String TAG = "NavActivity";
 
+    // Ui
+    private RecyclerView _recyclerView;
 
     @Override
+
     public int getLayoutResource() {
         return R.layout.activity_v3_nav;
     }
@@ -23,7 +32,12 @@ public class NavActivity extends AuthActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        _recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        _recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
+        _recyclerView.setAdapter(_recyclerView_adapter);
     }
+
 
     @Override
     public void onFinishCreate(Bundle savedInstanceState) {
@@ -31,8 +45,18 @@ public class NavActivity extends AuthActionBarActivity {
     }
 
     @Override
+    public int getToolbarId() {
+        return R.id.toolbar;
+    }
+
+    @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onProfile(Profile profile) {
+
     }
 
     @Override
@@ -45,6 +69,31 @@ public class NavActivity extends AuthActionBarActivity {
         super.onPause();
     }
 
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private final RecyclerView.Adapter<MyViewHolder> _recyclerView_adapter = new RecyclerView.Adapter<MyViewHolder>() {
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyViewHolder(new TextView(parent.getContext()));
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            TextView v = (TextView) holder.itemView;
+
+            v.setText(position + "");
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+    };
 
     public static void startNew(Context context) {
         Log.v(TAG, "startNew");
@@ -53,5 +102,4 @@ public class NavActivity extends AuthActionBarActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
-
 }
