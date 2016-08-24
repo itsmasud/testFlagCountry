d41cd912e46843af11efd6750e360e8bcd09021c
Michael Carver <michael.j.carver@gmail.com>
2016-08-24 08:21:17 -0500

    added and animation for the arrow, and cleaned up the other animation files.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/nav/NavActivity.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/nav/NavActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import com.fieldnation.R;
 import com.fieldnation.data.profile.Profile;
 import com.fieldnation.fnlog.Log;
 import com.fieldnation.ui.AuthSimpleActivity;
+import com.fieldnation.ui.IconFontTextView;
 
 /**
  * Created by Michael on 8/19/2016.
@@ -25,6 +28,11 @@ public class NavActivity extends AuthSimpleActivity {
     private RecyclerView _recyclerView;
     private Toolbar _toolbar;
     private View _searchesView;
+    private IconFontTextView _arrowTextView;
+
+    // Animations
+    private Animation _ccw;
+    private Animation _cw;
 
     @Override
     public int getLayoutResource() {
@@ -37,6 +45,8 @@ public class NavActivity extends AuthSimpleActivity {
 
         _toolbar = (Toolbar) findViewById(R.id.toolbar);
         _toolbar.setOnClickListener(_toolbar_onClick);
+
+        _arrowTextView = (IconFontTextView) findViewById(R.id.arrow_textview);
 
         _searchesView = findViewById(R.id.searchesView);
         _searchesView.setOnClickListener(new View.OnClickListener() {
@@ -51,12 +61,32 @@ public class NavActivity extends AuthSimpleActivity {
         _recyclerView.setAdapter(_recyclerView_adapter);
         _recyclerView.addOnScrollListener(_recycler_onScroll);
 
+        _ccw = AnimationUtils.loadAnimation(this, R.anim.rotate_180_ccw);
+        _ccw.setAnimationListener(_ccw_animationListener);
+        _cw = AnimationUtils.loadAnimation(this, R.anim.rotate_180_cw);
+        _cw.setAnimationListener(_ccw_animationListener);
         hideDrawer();
     }
 
+    private final Animation.AnimationListener _ccw_animationListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            _ccw.cancel();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+
     @Override
     public void onFinishCreate(Bundle savedInstanceState) {
-        setTitle(R.string.app_name);
     }
 
     @Override
@@ -65,38 +95,25 @@ public class NavActivity extends AuthSimpleActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
     public void onProfile(Profile profile) {
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
     }
 
     private void showDrawer() {
         if (_searchesView.getVisibility() != View.VISIBLE) {
             Log.v(TAG, "showDrawer");
-            setTitle("Field Nation \u25B2");
+            setTitle("Field Nation");
             _searchesView.setVisibility(View.VISIBLE);
+            _arrowTextView.startAnimation(_cw);
         }
     }
 
     private void hideDrawer() {
         if (_searchesView.getVisibility() != View.GONE) {
             Log.v(TAG, "hideDrawer");
-            setTitle("Field Nation \u25BC");
+            setTitle("Field Nation");
             _searchesView.setVisibility(View.GONE);
             _recyclerView.forceLayout();
+            _arrowTextView.startAnimation(_ccw);
         }
     }
 
@@ -111,6 +128,13 @@ public class NavActivity extends AuthSimpleActivity {
                 hideDrawer();
 
             }
+        }
+    };
+
+    private final RecyclerView.OnScrollListener _recycler_onScroll = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            hideDrawer();
         }
     };
 
@@ -137,13 +161,6 @@ public class NavActivity extends AuthSimpleActivity {
         @Override
         public int getItemCount() {
             return 200;
-        }
-    };
-
-    private final RecyclerView.OnScrollListener _recycler_onScroll = new RecyclerView.OnScrollListener() {
-        @Override
-        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            hideDrawer();
         }
     };
 
