e853064a1c7b11a3356cbcee35d22363127e476e
Michael Carver <michael.j.carver@gmail.com>
2016-10-24 11:27:23 -0500

    fixed? confirm activity, removed default username/pass

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/ncns/ConfirmActivity.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/ncns/ConfirmActivity.java
@@ -138,7 +138,7 @@ public class ConfirmActivity extends AuthSimpleActivity {
                 Action[] actions = wo.getPrimaryActions();
                 if (actions != null) {
                     for (Action a : actions) {
-                        if (a.getType() == Action.ActionType.CONFIRM) {
+                        if (a.getType() == Action.ActionType.READY) {
                             _doneButton.setVisibility(View.GONE);
                             return;
                         }
@@ -151,6 +151,7 @@ public class ConfirmActivity extends AuthSimpleActivity {
     private final View.OnClickListener _doneButton_onClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            App.get().setNeedsConfirmation(false);
             NavActivity.startNew(App.get());
         }
     };
