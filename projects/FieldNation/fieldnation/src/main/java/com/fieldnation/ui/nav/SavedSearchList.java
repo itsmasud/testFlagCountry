96c7fc016679f0301961f710eba8d62a28e9e0dc
Michael Carver <michael.carver@fieldnation.com>
2016-12-14 13:59:16 -0600

    PA-423 Fixed a thing

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/nav/SavedSearchList.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/nav/SavedSearchList.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.fieldnation.App;
 import com.fieldnation.R;
 import com.fieldnation.data.v2.SavedSearchParams;
 import com.fieldnation.fnlog.Log;
