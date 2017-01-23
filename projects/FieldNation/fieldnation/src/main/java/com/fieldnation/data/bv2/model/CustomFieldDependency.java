a0566dde5e5bb05ffa211b97273df5976143e937
Michael Carver <michael.carver@fieldnation.com>
2017-01-23 11:11:45 -0600

    PA-524 Working on the data model

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/data/bv2/model/CustomFieldDependency.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/data/bv2/model/CustomFieldDependency.java
@@ -1,6 +1,10 @@
 package com.fieldnation.data.bv2.model;
 
+import com.fieldnation.fnjson.JsonObject;
+import com.fieldnation.fnjson.Serializer;
+import com.fieldnation.fnjson.Unserializer;
 import com.fieldnation.fnjson.annotations.Json;
+import com.fieldnation.fnlog.Log;
 
 public class CustomFieldDependency {
     private static final String TAG = "CustomFieldDependency";
@@ -10,6 +14,9 @@ public class CustomFieldDependency {
 
     @Json(name = "value")
     private String value = null;
+
+    @Json(name = "operator")
+    private String operator = null;
 
     public enum OperatorEnum {
         LESS_THAN("less_than"),
@@ -28,111 +35,56 @@ public class CustomFieldDependency {
         public String toString() {
             return String.valueOf(value);
         }
+
+        public static OperatorEnum fromValue(String value) {
+            OperatorEnum[] values = values();
+            for (OperatorEnum e : values) {
+                if (e.value.equals(value))
+                    return e;
+            }
+            return null;
+        }
     }
 
-    @Json(name = "operator")
-    private OperatorEnum operator = null;
 
-    public CustomFieldDependency id(Integer id) {
-        this.id = id;
-        return this;
+    public CustomFieldDependency() {
     }
 
-    /**
-     * Get id
-     *
-     * @return id
-     **/
-    @ApiModelProperty(example = "null", value = "")
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public CustomFieldDependency value(String value) {
-        this.value = value;
-        return this;
-    }
-
-    /**
-     * Get value
-     *
-     * @return value
-     **/
-    @ApiModelProperty(example = "null", value = "")
     public String getValue() {
         return value;
     }
 
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public CustomFieldDependency operator(OperatorEnum operator) {
-        this.operator = operator;
-        return this;
-    }
-
-    /**
-     * Get operator
-     *
-     * @return operator
-     **/
-    @ApiModelProperty(example = "null", value = "")
     public OperatorEnum getOperator() {
-        return operator;
+        return OperatorEnum.fromValue(operator);
     }
 
-    public void setOperator(OperatorEnum operator) {
-        this.operator = operator;
-    }
-
-
-    @Override
-    public boolean equals(java.lang.Object o) {
-        if (this == o) {
-            return true;
+    /*-*****************************-*/
+    /*-             Json            -*/
+    /*-*****************************-*/
+    public static CustomFieldDependency fromJson(JsonObject obj) {
+        try {
+            return Unserializer.unserializeObject(CustomFieldDependency.class, obj);
+        } catch (Exception ex) {
+            Log.v(TAG, ex);
+            return null;
         }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
+    }
+
+    public JsonObject toJson() {
+        return toJson(this);
+    }
+
+    public static JsonObject toJson(CustomFieldDependency customFieldDependency) {
+        try {
+            return Serializer.serializeObject(customFieldDependency);
+        } catch (Exception ex) {
+            Log.v(TAG, ex);
+            return null;
         }
-        CustomFieldDependency customFieldDependency = (CustomFieldDependency) o;
-        return Objects.equals(this.id, customFieldDependency.id) &&
-                Objects.equals(this.value, customFieldDependency.value) &&
-                Objects.equals(this.operator, customFieldDependency.operator);
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, value, operator);
-    }
-
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class CustomFieldDependency {\n");
-
-        sb.append("    id: ").append(toIndentedString(id)).append("\n");
-        sb.append("    value: ").append(toIndentedString(value)).append("\n");
-        sb.append("    operator: ").append(toIndentedString(operator)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
-     */
-    private String toIndentedString(java.lang.Object o) {
-        if (o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
-
 }
 
