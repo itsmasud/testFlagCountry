151545048d9063839367d51b62a11ed9f08f172b
Michael Carver <michael.carver@fieldnation.com>
2017-02-16 14:35:06 -0600

    PA-607 Working on the dialog

---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/ui/dialog/ExpenseDialog.java
@@ -0,0 +1,24 @@
+package com.fieldnation.v2.ui.dialog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fieldnation.fndialog.SimpleDialog;
+
+/**
+ * Created by mc on 2/16/17.
+ */
+
+public class ExpenseDialog extends SimpleDialog {
+
+    public ExpenseDialog(Context context, ViewGroup container) {
+        super(context, container);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, Context context, ViewGroup container) {
+        return null;
+    }
+}
