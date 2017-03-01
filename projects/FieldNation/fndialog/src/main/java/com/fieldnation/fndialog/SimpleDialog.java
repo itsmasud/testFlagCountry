8b613c42c3549a438746d9d043daa2f2ca2fac6c
Michael Carver <michael.carver@fieldnation.com>
2017-03-01 08:49:56 -0600

    PA-606 Fixing bugs

--- projects/FieldNation/fndialog/src/main/java/com/fieldnation/fndialog/SimpleDialog.java
+++ projects/FieldNation/fndialog/src/main/java/com/fieldnation/fndialog/SimpleDialog.java
@@ -80,7 +80,7 @@ public abstract class SimpleDialog implements Dialog {
     public void onStart() {
         _clickBarrier.setOnClickListener(_this_onClick);
         _clickBarrier.setClickable(true);
-        _container.setClickable(true);
+        _container.setClickable(false);
     }
 
     @Override
