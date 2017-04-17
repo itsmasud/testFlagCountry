80005413b4e0db3f410985c3436f3bef9fc93f1d
SHOAIB AHMED <its.masud@yahoo.com>
2017-04-17 20:50:36 +0600

    PA-623
    except upload progressing, all other issues are fixed.

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/detail/TaskRowView.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/detail/TaskRowView.java
@@ -14,6 +14,8 @@ import com.fieldnation.fnlog.Log;
 import com.fieldnation.fntools.UniqueTag;
 import com.fieldnation.service.data.workorder.WorkorderClient;
 import com.fieldnation.ui.IconFontTextView;
+import com.fieldnation.v2.data.client.WorkordersWebApi;
+import com.fieldnation.v2.data.model.Error;
 import com.fieldnation.v2.data.model.Task;
 import com.fieldnation.v2.data.model.WorkOrder;
 
@@ -30,7 +32,7 @@ public class TaskRowView extends RelativeLayout {
 
     // Data
     private WorkOrder _workOrder;
-    private WorkorderClient _workorderClient;
+    private WorkordersWebApi _workOrderApi;
     private Task _task;
     private Listener _listener = null;
 
@@ -62,9 +64,6 @@ public class TaskRowView extends RelativeLayout {
         _descriptionTextView = (TextView) findViewById(R.id.description_textview);
         _progressBar = (ProgressBar) findViewById(R.id.progress_view);
 
-        _workorderClient = new WorkorderClient(_workorderClient_listener);
-        _workorderClient.connect(App.get());
-
         setOnClickListener(_checkbox_onClick);
 
         populateUi();
@@ -72,9 +71,11 @@ public class TaskRowView extends RelativeLayout {
 
     @Override
     protected void onDetachedFromWindow() {
-        if (_workorderClient != null && _workorderClient.isConnected())
-            _workorderClient.disconnect(App.get());
-        _workorderClient = null;
+        if (_workOrderApi != null && _workOrderApi.isConnected())
+            _workOrderApi.disconnect(App.get());
+        _workOrderApi = null;
+
+
         super.onDetachedFromWindow();
     }
 
@@ -119,6 +120,9 @@ public class TaskRowView extends RelativeLayout {
             return;
 
         if (_uploadingFiles.size() > 0) {
+
+
+            Log.e(TAG, "inside _uploadingFiles");
 
             if (_uploadingFiles.size() == 1) {
                 _descriptionTextView.setText(_task.getType().getName() + "\nUploading: " + _uploadingFiles.iterator().next());
@@ -177,8 +181,7 @@ public class TaskRowView extends RelativeLayout {
     private void updateCheckBox() {
         if (_task != null && _task.getActionsSet() != null
                 && (_task.getActionsSet().contains(Task.ActionsEnum.EDIT)
-                || _task.getActionsSet().contains(Task.ActionsEnum.COMPLETE)
-                || _task.getActionsSet().contains(Task.ActionsEnum.INCOMPLETE))) {
+                || _task.getActionsSet().contains(Task.ActionsEnum.COMPLETE))) {
             _descriptionTextView.setTextColor(getResources().getColor(R.color.fn_dark_text));
             setEnabled(true);
         } else {
@@ -203,50 +206,43 @@ public class TaskRowView extends RelativeLayout {
         if (_workOrder == null)
             return;
 
-        if (_task == null || _task.getAttachments() == null || _task.getAttachments().getId() == null)
-            return;
-
-        if (_workorderClient == null)
-            return;
-
-        if (!_workorderClient.isConnected())
-            return;
-
-        _workorderClient.subDeliverableUpload(_workOrder.getId(), _task.getAttachments().getId());
-        _workorderClient.subDeliverableProgress(_workOrder.getId(), _task.getAttachments().getId());
+        if (_workOrderApi == null || !_workOrderApi.isConnected()) {
+            _workOrderApi = new WorkordersWebApi(_workOrderApi_listener);
+            _workOrderApi.connect(App.get());
+        }
     }
 
-    private final WorkorderClient.Listener _workorderClient_listener = new WorkorderClient.Listener() {
-        @Override
-        public void onConnected() {
-            subscribeUpload();
-        }
+//    private final WorkorderClient.Listener _workorderClient_listener = new WorkorderClient.Listener() {
+//        @Override
+//        public void onConnected() {
+//            subscribeUpload();
+//        }
+//
+//        @Override
+//        public void onUploadDeliverable(long workorderId, long slotId, String filename, boolean isComplete, boolean failed) {
+//            if (failed || isComplete) {
+//                _uploadingFiles.remove(filename);
+//                _uploadingProgress.put(filename, 100);
+//
+//                if (_uploadingFiles.size() == 0)
+//                    _uploadingProgress.clear();
+//            } else {
+//                _uploadingFiles.add(filename);
+//                if (!_uploadingProgress.containsKey(filename))
+//                    _uploadingProgress.put(filename, 0);
+//            }
+//            populateUi();
+//        }
 
-        @Override
-        public void onUploadDeliverable(long workorderId, long slotId, String filename, boolean isComplete, boolean failed) {
-            if (failed || isComplete) {
-                _uploadingFiles.remove(filename);
-                _uploadingProgress.put(filename, 100);
-
-                if (_uploadingFiles.size() == 0)
-                    _uploadingProgress.clear();
-            } else {
-                _uploadingFiles.add(filename);
-                if (!_uploadingProgress.containsKey(filename))
-                    _uploadingProgress.put(filename, 0);
-            }
-            populateUi();
-        }
-
-        @Override
-        public void onUploadDeliverableProgress(long workorderId, long slotId, String filename, long pos, long size, long time) {
-            Double percent = pos * 1.0 / size;
-            Log.v(TAG, "onUploadDeliverableProgress(" + workorderId + "," + slotId + "," + filename + "," + (pos * 100 / size) + "," + (int) (time / percent));
-            int prog = (int) (pos * 100 / size);
-            _uploadingProgress.put(filename, prog);
-            populateUi();
-        }
-    };
+//        @Override
+//        public void onUploadDeliverableProgress(long workorderId, long slotId, String filename, long pos, long size, long time) {
+//            Double percent = pos * 1.0 / size;
+//            Log.v(TAG, "onUploadDeliverableProgress(" + workorderId + "," + slotId + "," + filename + "," + (pos * 100 / size) + "," + (int) (time / percent));
+//            int prog = (int) (pos * 100 / size);
+//            _uploadingProgress.put(filename, prog);
+//            populateUi();
+//        }
+//    };
 
     private final View.OnClickListener _checkbox_onClick = new View.OnClickListener() {
         @Override
@@ -254,10 +250,51 @@ public class TaskRowView extends RelativeLayout {
             updateCheckBox();
 
             if (_listener != null) {
+//            Double percent = pos * 1.0 / size;
+//            Log.v(TAG, "onUploadDeliverableProgress(" + workorderId + "," + slotId + "," + filename + "," + (pos * 100 / size) + "," + (int) (time / percent));
+//            int prog = (int) (pos * 100 / size);
+                _uploadingProgress.put("task" + _task.getId(), 10);
+                populateUi();
+//        }
+
                 _listener.onTaskClick(_task);
+
+
+
             }
         }
     };
+
+
+    private final WorkordersWebApi.Listener _workOrderApi_listener = new WorkordersWebApi.Listener() {
+        @Override
+        public void onConnected() {
+            _workOrderApi.subWorkordersWebApi();
+        }
+
+        @Override
+        public void onWorkordersWebApi(String methodName, Object successObject, boolean success, Object failObject) {
+
+            if (methodName.contains("addAttachment") && success) {
+            if (success) {
+                _uploadingFiles.remove("task" + _task.getId());
+                _uploadingProgress.put("done", 100);
+
+                if (_uploadingFiles.size() == 0)
+                    _uploadingProgress.clear();
+            }
+
+//            else {
+//                _uploadingFiles.add(filename);
+//                if (!_uploadingProgress.containsKey(filename))
+//                    _uploadingProgress.put(filename, 0);
+//            }
+            populateUi();
+            }
+
+        }
+    };
+
 
     public interface Listener {
         void onTaskClick(Task task);
