91ee891e540b2605ec654af2041b4410b4b781cc
Michael Carver <michael.carver@fieldnation.com>
2017-05-02 16:32:47 -0500

    Fixing some minor issues around messages, and eta

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/detail/MessageFragment.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/detail/MessageFragment.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import com.fieldnation.App;
 import com.fieldnation.R;
 import com.fieldnation.analytics.trackers.WorkOrderTracker;
+import com.fieldnation.fnjson.JsonObject;
 import com.fieldnation.fnlog.Log;
 import com.fieldnation.fntoast.ToastClient;
 import com.fieldnation.fntools.misc;
@@ -198,35 +199,47 @@ public class MessageFragment extends WorkorderFragment {
 
         @Override
         public void onComplete(TransactionParams transactionParams, String methodName, Object successObject, boolean success, Object failObject) {
-            if (successObject instanceof Messages) {
-                Messages messages = (Messages) successObject;
-                Error error = (Error) failObject;
-                if (!success || error != null)
-                    return;
+            try {
+                if (successObject instanceof Messages) {
+                    Messages messages = (Messages) successObject;
+                    Error error = (Error) failObject;
+                    if (!success || error != null)
+                        return;
 
-                if (messages == null || messages.getResults() == null) {
-                    _refreshView.refreshComplete();
-                    return;
-                }
+                    JsonObject methodParams = new JsonObject(transactionParams.methodParams);
 
-                List<Message> results = Arrays.asList(messages.getResults());
-
-                Collections.sort(results, new Comparator<Message>() {
-                    @Override
-                    public int compare(Message lhs, Message rhs) {
-                        try {
-                            return (int) (lhs.getCreated().getUtcLong() - rhs.getCreated().getUtcLong());
-                        } catch (Exception ex) {
-                            return 0;
-                        }
+                    if (methodParams.has("workOrderId") && _workorder != null
+                            && methodParams.getInt("workOrderId") != _workorder.getId()) {
+                        Log.v(TAG, "not my work order!");
+                        return;
                     }
-                });
 
-                _adapter.addObjects(messages.getMetadata().getPage(), results);
+                    if (messages == null || messages.getResults() == null || _workorder == null) {
+                        _refreshView.refreshComplete();
+                        return;
+                    }
 
-                rebuildList();
-            } else if (successObject instanceof Message) {
-                rebuildList();
+                    List<Message> results = Arrays.asList(messages.getResults());
+
+                    Collections.sort(results, new Comparator<Message>() {
+                        @Override
+                        public int compare(Message lhs, Message rhs) {
+                            try {
+                                return (int) (lhs.getCreated().getUtcLong() - rhs.getCreated().getUtcLong());
+                            } catch (Exception ex) {
+                                return 0;
+                            }
+                        }
+                    });
+
+                    _adapter.addObjects(messages.getMetadata().getPage(), results);
+
+                    rebuildList();
+                } else if (successObject instanceof Message) {
+                    rebuildList();
+                }
+            } catch (Exception ex) {
+                Log.v(TAG, ex);
             }
         }
     };
