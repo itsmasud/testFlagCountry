ad6800d0ce5e3bdc827008d7633634ca0fc4bf1e
Michael Carver <michael.carver@fieldnation.com>
2017-05-10 10:32:47 -0500

    PA-362 added NoSwipeViewPager to remove swipe to change tabs

---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/NoSwipeViewPager.java
@@ -0,0 +1,30 @@
+package com.fieldnation.ui;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by mc on 5/10/17.
+ */
+
+public class NoSwipeViewPager extends ViewPager {
+    public NoSwipeViewPager(Context context) {
+        super(context);
+    }
+
+    public NoSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+}
