9d47221a649a6d17e35b668db6e7b7a5d214c0d9
Michael Carver <michael.carver@fieldnation.com>
2017-05-18 11:23:39 -0500

    PA-756 Fixed bug

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/detail/ExpectedPaymentView.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/ui/workorder/detail/ExpectedPaymentView.java
@@ -95,7 +95,10 @@ public class ExpectedPaymentView extends LinearLayout implements WorkOrderRender
 
         Pay pay = _workOrder.getPay();
 
-        if (pay == null || pay.getBase() == null || pay.getBase().getAmount() == null || pay.getBase().getAmount() == 0) {
+        if (pay == null
+                || pay.getBase() == null
+                || pay.getBase().getAmount() == null
+                || pay.getBase().getAmount() == 0) {
             this.setVisibility(GONE);
             return;
         } else {
