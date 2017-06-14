fb69a8346709e5d0c0442413a0c63db1f0c828be
SHOAIB AHMED <its.masud@yahoo.com>
2017-06-14 20:14:08 +0600

    PA-755
    Almost done. Working fine.

---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/ui/dialog/ExpensesCoCardView.java
@@ -0,0 +1,85 @@
+package com.fieldnation.v2.ui.dialog;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import com.fieldnation.R;
+import com.fieldnation.fnlog.Log;
+import com.fieldnation.fntools.misc;
+import com.fieldnation.v2.data.model.Expense;
+
+/**
+ * Created by shoaib.ahmed on 11/5/2014.
+ */
+public class ExpensesCoCardView extends RelativeLayout {
+    private static final String TAG = "ExpensesCoCardView";
+
+    // Ui
+    private EditText _descriptionEditText;
+    private EditText _amountEditText;
+
+
+    /*-*****************************-*/
+    /*-         Life Cycle          -*/
+    /*-*****************************-*/
+
+    public ExpensesCoCardView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ExpensesCoCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ExpensesCoCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.view_co_expenses_card, this);
+
+        if (isInEditMode())
+            return;
+
+        _descriptionEditText = (EditText) findViewById(R.id.description_edittext);
+        _amountEditText = (EditText) findViewById(R.id.amount_edittext);
+    }
+
+    public Expense getExpense() {
+        try {
+            return new Expense().description(getDescription()).amount(getAmount());
+        } catch (Exception ex) {
+            Log.v(TAG, ex);
+        }
+        return null;
+    }
+
+    public boolean isValidExpense() {
+        return !misc.isEmptyOrNull(getDescription()) && getAmount() != 0;
+    }
+
+    public void setExpense(Expense expense) {
+        _descriptionEditText.setText(expense.getDescription());
+        _amountEditText.setText(expense.getAmount().toString());
+    }
+
+
+    private String getDescription() {
+        return _descriptionEditText.getText().toString().trim();
+    }
+
+    private Double getAmount() {
+        try {
+            return Double.parseDouble(_amountEditText.getText().toString());
+        } catch (Exception ex) {
+            return 0.0;
+        }
+    }
+
+}
--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/ui/dialog/PaymentCoView.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/ui/dialog/PaymentCoView.java
@@ -1,39 +1,65 @@
 package com.fieldnation.v2.ui.dialog;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.fieldnation.R;
 import com.fieldnation.fnlog.Log;
 import com.fieldnation.fntools.misc;
+import com.fieldnation.ui.HintArrayAdapter;
+import com.fieldnation.ui.HintSpinner;
 import com.fieldnation.v2.data.model.Pay;
+import com.fieldnation.v2.data.model.PayAdditional;
+import com.fieldnation.v2.data.model.PayBase;
 
 /**
- * Created by michael.carver on 11/5/2014.
+ * Created by shoaib.ahmed on 30/5/2017.
  */
 public class PaymentCoView extends RelativeLayout {
-    private static final String TAG = "PaymentCoView";
+    private static String TAG = "PaymentCoView";
 
-    // Ui
-    private TextView _statusTextView;
-    private TextView _body1TextView;
-    private TextView _body2TextView;
-    private Button _clearButton;
-    private Button _changeButton;
+
+    private static final double MINIMUM_ACCUMULATED_PAYABLE_AMOUNT = 20;
+    private static final double MINIMUM_PAYABLE_AMOUNT = .1;
+
+    // Modes
+    private static final int MODE_FIXED = 0;
+    private static final int MODE_HOURLY = 1;
+    private static final int MODE_PER_DEVICE = 2;
+    private static final int MODE_BLENDED = 3;
+
+    // UI
+    private TextView _payTypeTitleTextView;
+    private HintSpinner _typeSpinner;
+
+    private LinearLayout _fixedLayout;
+    private EditText _fixedEditText;
+
+    private LinearLayout _hourlyLayout;
+    private EditText _hourlyRateEditText;
+    private EditText _maxHoursEditText;
+
+    private LinearLayout _devicesLayout;
+    private EditText _deviceRateEditText;
+    private EditText _maxDevicesEditText;
+
+    private LinearLayout _blendedLayout;
+    private EditText _blendedFixedRateEditText;
+    private EditText _blendedFixedMaxHoursEditText;
+    private EditText _extraHourlyEditText;
+    private EditText _extraMaxHoursEditText;
 
     // Data
-    private boolean _isCounterOffer;
-    private Pay _pay;
-    private Listener _listener;
-
-    /*-*****************************-*/
-    /*-         Life Cycle          -*/
-    /*-*****************************-*/
+    private int _mode = -1;
 
     public PaymentCoView(Context context) {
         super(context);
@@ -51,97 +77,286 @@ public class PaymentCoView extends RelativeLayout {
     }
 
     private void init() {
-        LayoutInflater.from(getContext()).inflate(R.layout.view_co_payment_tile, this);
+        Log.e(TAG, "init");
+        LayoutInflater.from(getContext()).inflate(R.layout.view_co_pay_new, this);
 
         if (isInEditMode())
             return;
 
-        _statusTextView = (TextView) findViewById(R.id.status_textview);
-        _body1TextView = (TextView) findViewById(R.id.body1_textview);
-        _body2TextView = (TextView) findViewById(R.id.body2_textview);
+        _payTypeTitleTextView = (TextView) findViewById(R.id.pay_type_title_textview);
 
-        _clearButton = (Button) findViewById(R.id.clear_button);
-        _clearButton.setOnClickListener(_clear_onClick);
-        _changeButton = (Button) findViewById(R.id.change_button);
-        _changeButton.setOnClickListener(_change_onClick);
-    }
+        _typeSpinner = (HintSpinner) findViewById(R.id.type_spinner);
+        _typeSpinner.setOnItemSelectedListener(_type_selected);
+        getTypeSpinner();
 
-    public void setListener(Listener listener) {
-        _listener = listener;
-    }
+        // fixed
+        _fixedLayout = (LinearLayout) findViewById(R.id.fixed_layout);
+        _fixedEditText = (EditText) findViewById(R.id.fixed_edittext);
+        _fixedEditText.addTextChangedListener(_textWatcher_listener);
 
-    public void setPay(Pay pay, boolean isCounterOffer) {
-        _pay = pay;
-        _isCounterOffer = isCounterOffer;
+        // hourly
+        _hourlyLayout = (LinearLayout) findViewById(R.id.hourly_layout);
+        _hourlyRateEditText = (EditText) findViewById(R.id.hourlyrate_edittext);
+        _maxHoursEditText = (EditText) findViewById(R.id.maxhours_edittext);
+
+        // per device
+        _devicesLayout = (LinearLayout) findViewById(R.id.devices_layout);
+        _deviceRateEditText = (EditText) findViewById(R.id.devicerate_edittext);
+        _maxDevicesEditText = (EditText) findViewById(R.id.maxdevices_edittext);
+
+        // blended
+        _blendedLayout = (LinearLayout) findViewById(R.id.blended_layout);
+        _blendedFixedRateEditText = (EditText) findViewById(R.id.blendedFixedRate_edittext);
+        _blendedFixedMaxHoursEditText = (EditText) findViewById(R.id.blendedFixedMaxHours_edittext);
+        _extraHourlyEditText = (EditText) findViewById(R.id.extrahours_edittext);
+        _extraMaxHoursEditText = (EditText) findViewById(R.id.extramaxhours_edittext);
+
         populateUi();
     }
 
-    public Pay getPay() {
-        return _pay;
+
+    private HintSpinner getTypeSpinner() {
+        if (_typeSpinner != null && _typeSpinner.getAdapter() == null) {
+            HintArrayAdapter adapter = HintArrayAdapter.createFromResources(
+                    _typeSpinner.getContext(),
+                    R.array.pay_types,
+                    R.layout.view_spinner_item_gray);
+
+            adapter.setDropDownViewResource(
+                    android.support.design.R.layout.support_simple_spinner_dropdown_item);
+
+            _typeSpinner.setAdapter(adapter);
+        }
+        return _typeSpinner;
     }
 
-    private void populateUi() {
-        if (_pay == null)
-            return;
 
-        _body1TextView.setVisibility(VISIBLE);
-        _body2TextView.setVisibility(View.GONE);
-
+    private Double getDouble(String _amount) {
         try {
-            switch (_pay.getType()) {
-                case BLENDED:
-                    _body2TextView.setVisibility(View.VISIBLE);
-                    _body1TextView.setText("Blended " + misc.toCurrencyTrim(_pay.getBase().getAmount()) + " for the first " + _pay.getBase().getUnits() + " hours");
-                    _body2TextView.setText(misc.toCurrencyTrim(_pay.getAdditional().getAmount()) + "/hour for up to " + _pay.getAdditional().getUnits() + " hours");
-                    break;
-                case DEVICE:
-                    _body1TextView.setText(misc.toCurrencyTrim(_pay.getBase().getAmount()) + "/device for up to " + _pay.getBase().getUnits() + " devices");
-                    break;
-                case FIXED:
-                    _body1TextView.setText("Fixed pay of " + misc.toCurrencyTrim(_pay.getBase().getAmount()));
-                    break;
-                case HOURLY:
-                    _body1TextView.setText("Hourly pay, " + misc.toCurrencyTrim(_pay.getBase().getAmount()) + "/hour for up to " + _pay.getBase().getUnits() + " hours");
-                    break;
-            }
-
-            _statusTextView.setVisibility(VISIBLE);
-            if (_isCounterOffer) {
-                _statusTextView.setText("Your Offer");
+            double amount = Double.parseDouble(_amount);
+            if ((int) (amount * 100) < 10) {
+                return 0.0;
             } else {
-                _statusTextView.setText("Buyer's Offer");
+                return amount;
+            }
+        } catch (Exception ex) {
+            return 0.0;
+        }
+    }
+
+    private Integer getInteger(String _noStr) {
+        try {
+            int _no = Integer.parseInt(_noStr);
+            return _no;
+        } catch (Exception ex) {
+            return getDouble(_noStr).intValue();
+        }
+    }
+
+    public boolean isValidAmount() {
+        try {
+            switch (_mode) {
+                case MODE_FIXED:
+                    return getDouble(_fixedEditText.getText().toString()) >= MINIMUM_PAYABLE_AMOUNT;
+                case MODE_HOURLY:
+                    double hourlyRateAmount = getDouble((_hourlyRateEditText.getText().toString()));
+                    return hourlyRateAmount >= MINIMUM_PAYABLE_AMOUNT;
+                case MODE_PER_DEVICE:
+                    double deviceRate = getDouble((_deviceRateEditText.getText().toString()));
+                    return deviceRate >= MINIMUM_PAYABLE_AMOUNT;
+                case MODE_BLENDED:
+                    double blendedFixedRate = getDouble((_blendedFixedRateEditText.getText().toString()));
+//                    double blendedMaxHours = getDouble((_blendedFixedMaxHoursEditText.getText().toString()));
+                    double extraHourly = getDouble((_extraHourlyEditText.getText().toString()));
+                    return blendedFixedRate >= MINIMUM_PAYABLE_AMOUNT && extraHourly >= MINIMUM_PAYABLE_AMOUNT;
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return false;
+    }
+
+
+    public boolean isValidTotalAmount() {
+        try {
+            switch (_mode) {
+                case MODE_FIXED:
+                    return getDouble(_fixedEditText.getText().toString()) > MINIMUM_ACCUMULATED_PAYABLE_AMOUNT;
+                case MODE_HOURLY:
+                    double hourlyRateAmount = getDouble((_hourlyRateEditText.getText().toString()));
+                    double maxHours = getDouble((_maxHoursEditText.getText().toString()));
+                    return hourlyRateAmount * maxHours > MINIMUM_ACCUMULATED_PAYABLE_AMOUNT;
+                case MODE_PER_DEVICE:
+                    double deviceRate = getDouble((_deviceRateEditText.getText().toString()));
+                    int maxDevices = getInteger((_maxDevicesEditText.getText().toString()));
+                    return deviceRate * maxDevices > MINIMUM_ACCUMULATED_PAYABLE_AMOUNT;
+                case MODE_BLENDED:
+                    double blendedFixedRate = getDouble((_blendedFixedRateEditText.getText().toString()));
+//                    double blendedMaxHours = getDouble((_blendedFixedMaxHoursEditText.getText().toString()));
+                    double extraHourly = getDouble((_extraHourlyEditText.getText().toString()));
+                    double extraMaxHours = getDouble((_extraMaxHoursEditText.getText().toString()));
+                    return blendedFixedRate + (extraHourly * extraMaxHours) > MINIMUM_ACCUMULATED_PAYABLE_AMOUNT;
+            }
+        } catch (Exception ex) {
+            return false;
+        }
+        return false;
+    }
+
+
+    public boolean isValidPay() {
+        if (_mode == -1) return false;
+
+//        try {
+//            switch (_mode) {
+//                case MODE_FIXED:
+//                    if (misc.isEmptyOrNull(_fixedEditText.getText().toString())) return false;
+//                    break;
+//                case MODE_HOURLY:
+//                    if (misc.isEmptyOrNull(_hourlyRateEditText.getText().toString()) || misc.isEmptyOrNull(_maxHoursEditText.getText().toString()))
+//                        return false;
+//                    break;
+//                case MODE_PER_DEVICE:
+//                    if (misc.isEmptyOrNull(_deviceRateEditText.getText().toString()) || misc.isEmptyOrNull(_maxDevicesEditText.getText().toString()))
+//                        return false;
+//                    break;
+//                case MODE_BLENDED:
+//                    if (misc.isEmptyOrNull(_blendedFixedRateEditText.getText().toString())
+//                            || misc.isEmptyOrNull(_blendedFixedMaxHoursEditText.getText().toString())
+//                            || misc.isEmptyOrNull(_extraHourlyEditText.getText().toString())
+//                            || misc.isEmptyOrNull(_extraMaxHoursEditText.getText().toString()))
+//                        return false;
+//
+//                    break;
+//            }
+//        } catch (Exception ex) {
+//            Log.v(TAG, ex);
+//        }
+        return true;
+    }
+
+    public Pay getPay() {
+        Pay pay = new Pay();
+        try {
+            switch (_mode) {
+                case MODE_FIXED:
+                    pay.setType(Pay.TypeEnum.FIXED);
+                    pay.setBase(new PayBase()
+                            .amount(misc.isEmptyOrNull(_fixedEditText.getText().toString()) ? 0.0 : Double.parseDouble(_fixedEditText.getText().toString()))
+                            .units(1.0));
+                    break;
+                case MODE_HOURLY:
+                    pay.setType(Pay.TypeEnum.HOURLY);
+                    pay.setBase(new PayBase()
+                            .amount(misc.isEmptyOrNull(_hourlyRateEditText.getText().toString()) ? 0.0 : Double.parseDouble(_hourlyRateEditText.getText().toString()))
+                            .units(misc.isEmptyOrNull(_maxHoursEditText.getText().toString()) ? 0.0 : Double.parseDouble(_maxHoursEditText.getText().toString())));
+                    break;
+                case MODE_PER_DEVICE:
+                    pay.setType(Pay.TypeEnum.DEVICE);
+                    pay.setBase(new PayBase()
+                            .amount(misc.isEmptyOrNull(_deviceRateEditText.getText().toString()) ? 0.0 : Double.parseDouble(_deviceRateEditText.getText().toString()))
+                            .units(misc.isEmptyOrNull(_maxDevicesEditText.getText().toString()) ? 0.0 : Double.parseDouble(_maxDevicesEditText.getText().toString())));
+                    break;
+                case MODE_BLENDED:
+                    pay.setType(Pay.TypeEnum.BLENDED);
+                    pay.setBase(new PayBase()
+                            .amount(misc.isEmptyOrNull(_blendedFixedRateEditText.getText().toString()) ? 0.0 : Double.parseDouble(_blendedFixedRateEditText.getText().toString()))
+                            .units(misc.isEmptyOrNull(_blendedFixedMaxHoursEditText.getText().toString()) ? 0.0 : Double.parseDouble(_blendedFixedMaxHoursEditText.getText().toString())));
+                    pay.setAdditional(new PayAdditional()
+                            .amount(misc.isEmptyOrNull(_extraHourlyEditText.getText().toString()) ? 0.0 : Double.parseDouble(_extraHourlyEditText.getText().toString()))
+                            .units(misc.isEmptyOrNull(_extraMaxHoursEditText.getText().toString()) ? 0.0 : Double.parseDouble(_extraMaxHoursEditText.getText().toString())));
+                    break;
             }
         } catch (Exception ex) {
             Log.v(TAG, ex);
-            _statusTextView.setVisibility(GONE);
-            _body1TextView.setVisibility(GONE);
-            _body2TextView.setVisibility(GONE);
         }
-
+        return pay;
     }
 
-    /*-*************************-*/
-    /*-         Events          -*/
-    /*-*************************-*/
-    private final View.OnClickListener _clear_onClick = new View.OnClickListener() {
+    private void setMode(int mode) {
+        _mode = mode;
+
+        switch (mode) {
+            case MODE_FIXED:
+                _blendedLayout.setVisibility(View.GONE);
+                _devicesLayout.setVisibility(View.GONE);
+                _fixedLayout.setVisibility(View.VISIBLE);
+                _hourlyLayout.setVisibility(View.GONE);
+                _payTypeTitleTextView.setVisibility(VISIBLE);
+                break;
+            case MODE_HOURLY:
+                _blendedLayout.setVisibility(View.GONE);
+                _devicesLayout.setVisibility(View.GONE);
+                _fixedLayout.setVisibility(View.GONE);
+                _hourlyLayout.setVisibility(View.VISIBLE);
+                _payTypeTitleTextView.setVisibility(VISIBLE);
+                break;
+            case MODE_PER_DEVICE:
+                _blendedLayout.setVisibility(View.GONE);
+                _devicesLayout.setVisibility(View.VISIBLE);
+                _fixedLayout.setVisibility(View.GONE);
+                _hourlyLayout.setVisibility(View.GONE);
+                _payTypeTitleTextView.setVisibility(VISIBLE);
+                break;
+            case MODE_BLENDED:
+                _blendedLayout.setVisibility(View.VISIBLE);
+                _devicesLayout.setVisibility(View.GONE);
+                _fixedLayout.setVisibility(View.GONE);
+                _hourlyLayout.setVisibility(View.GONE);
+                _payTypeTitleTextView.setVisibility(VISIBLE);
+                break;
+            case -1:
+                _blendedLayout.setVisibility(View.GONE);
+                _devicesLayout.setVisibility(View.GONE);
+                _fixedLayout.setVisibility(View.GONE);
+                _hourlyLayout.setVisibility(View.GONE);
+                _payTypeTitleTextView.setVisibility(GONE);
+
+
+        }
+    }
+
+    private void populateUi() {
+        Log.e(TAG, "populateUi");
+        setMode(_mode);
+    }
+
+    /*-*********************************-*/
+    /*-				Events				-*/
+    /*-*********************************-*/
+    private final AdapterView.OnItemSelectedListener _type_selected = new AdapterView.OnItemSelectedListener() {
         @Override
-        public void onClick(View v) {
-            if (_listener != null)
-                _listener.onClearClick();
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            setMode(position);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            _mode = -1;
         }
     };
 
-    private final View.OnClickListener _change_onClick = new View.OnClickListener() {
+    private final TextWatcher _textWatcher_listener = new TextWatcher() {
         @Override
-        public void onClick(View v) {
-            if (_listener != null)
-                _listener.onChangeClick(_pay);
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+//            if (!misc.isEmptyOrNull(_fixedEditText.getText().toString())) {
+//                _fixedEditText.setText("$" + _fixedEditText.getText().toString());
+//            }
+//
+//            Log.e(TAG, "text: " + s);
+
+
         }
     };
 
-    public interface Listener {
-        void onClearClick();
 
-        void onChangeClick(Pay pay);
-    }
 }
