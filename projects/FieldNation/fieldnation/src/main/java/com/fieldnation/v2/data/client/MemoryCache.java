158e194b2fc5136d45042f872219db1679c7cc24
Michael Carver <michael.carver@fieldnation.com>
2017-08-10 15:51:34 -0500

    Fixed cached being cleaned out early

---
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/data/client/MemoryCache.java
@@ -0,0 +1,52 @@
+package com.fieldnation.v2.data.client;
+
+import java.util.Hashtable;
+import java.util.Set;
+
+/**
+ * Created by mc on 8/10/17.
+ */
+
+public class MemoryCache {
+    private static final String TAG = "MemoryCache";
+    private static Hashtable<Integer, Node> CACHE = new Hashtable<>();
+    private static int CACHE_NEXT = 0;
+
+    private static class Node {
+        long ttl;
+        Object object;
+
+        public Node(Object object) {
+            this.object = object;
+            ttl = System.currentTimeMillis() + 5000;
+        }
+    }
+
+    public static int put(Object object) {
+        synchronized (TAG) {
+            int id = CACHE_NEXT;
+            CACHE.put(id, new Node(object));
+            CACHE_NEXT++;
+            return id;
+        }
+    }
+
+    public static Object get(int key) {
+        synchronized (TAG) {
+            Object value = CACHE.get(key).object;
+            purgeNodes();
+            return value;
+        }
+    }
+
+    private static void purgeNodes() {
+        Set<Integer> keys = CACHE.keySet();
+        for (Integer key : keys) {
+            Node node = CACHE.get(key);
+            if (node.ttl < System.currentTimeMillis()) {
+                CACHE.remove(key);
+            }
+        }
+    }
+
+}
