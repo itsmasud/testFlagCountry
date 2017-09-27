f75db6accbbba2bd8e2df7adba5462b18e3b13ba
Michael Carver <michael.carver@fieldnation.com>
2017-09-27 14:50:55 -0500

    MAR-550 Added new view

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/ui/dialog/PhotoUploadDialog.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/v2/ui/dialog/PhotoUploadDialog.java
@@ -18,6 +18,7 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -73,6 +74,8 @@ public class PhotoUploadDialog extends FullScreenDialog {
     private EditText _fileNameEditText;
     private EditText _descriptionEditText;
     private ProgressBar _progressBar;
+    private RelativeLayout _noPreviewLayout;
+    private TextView _iconTextView;
 
     // Data user entered
     private String _newFileName;
@@ -94,6 +97,116 @@ public class PhotoUploadDialog extends FullScreenDialog {
     private TransactionParams _transactionParams;
     private JsonObject _methodParams;
     private JsonObject _httpBuilder;
+
+
+    private static int getIcon(String ext) {
+        switch (ext) {
+            // audio
+            case ".aa":
+            case ".aac":
+            case ".act":
+            case ".aiff":
+            case ".amr":
+            case ".ape":
+            case ".au":
+            case ".awb":
+            case ".dct":
+            case ".dvf":
+            case ".flac":
+            case ".gsm":
+            case ".iklax":
+            case ".ivs":
+            case ".m4a":
+            case ".m4b":
+            case ".m4p":
+            case ".mmf":
+            case ".mp3":
+            case ".mpc":
+            case ".msv":
+            case ".ogg":
+            case ".oga":
+            case ".mogg":
+            case ".opus":
+            case ".ra":
+            case ".rm":
+            case ".raw":
+            case ".sln":
+            case ".tta":
+            case ".vox":
+            case ".wav":
+            case ".wma":
+            case ".wv":
+            case ".webm":
+            case ".8svx":
+            case ".midi":
+
+                return R.string.icon_file_generic;
+
+            // code
+            case ".php":
+            case ".xml":
+            case ".c":
+            case ".bat":
+            case ".cpp":
+            case ".cs":
+            case ".java":
+            case ".html":
+            case ".js":
+                return R.string.icon_file_generic;
+
+            // spreadsheet
+            case ".xls":
+            case ".ods":
+            case ".wks":
+            case ".wku":
+            case ".wq1":
+            case ".wq2":
+            case ".xlr":
+            case ".xlsb":
+            case ".xlsm":
+            case ".xlshtml":
+            case ".xlsmhtml":
+            case ".xlsx":
+            case ".xlthtml":
+            case ".xltm":
+            case ".xltx":
+                return R.string.icon_file_generic;
+
+            // text
+            case ".txt":
+            case ".pdf":
+            case ".doc":
+            case ".docx":
+            case ".rtf":
+                return R.string.icon_file_generic;
+
+            // video
+            case ".264":
+            case ".3g2":
+            case ".3gp2":
+            case ".3gpp":
+            case ".3gpp2":
+            case ".aaf":
+            case ".amv":
+            case ".avi":
+            case ".m2v":
+            case ".mov":
+            case ".mp4":
+            case ".mpeg":
+                return R.string.icon_file_generic;
+
+            // zip
+            case ".zip":
+            case ".rar":
+            case ".7z":
+            case ".arc":
+            case ".jar":
+                return R.string.icon_file_png;
+
+            default:
+                return R.string.icon_doc_generic;
+        }
+    }
 
     /*-*************************************-*/
     /*-				Life Cycle				-*/
@@ -117,6 +230,8 @@ public class PhotoUploadDialog extends FullScreenDialog {
         _fileNameEditText = v.findViewById(R.id.filename_edittext);
         _descriptionEditText = v.findViewById(R.id.description_edittext);
         _progressBar = v.findViewById(R.id.progressBar);
+        _noPreviewLayout = v.findViewById(R.id.previewUnavailable_view);
+        _iconTextView = v.findViewById(R.id.icon_textview);
 
         return v;
     }
@@ -249,12 +364,16 @@ public class PhotoUploadDialog extends FullScreenDialog {
             _progressBar.setVisibility(View.GONE);
             _imageView.setVisibility(View.VISIBLE);
             _imageView.setImageBitmap(_bitmap);
+            _noPreviewLayout.setVisibility(View.GONE);
         } else if (_hideImageView) {
             _progressBar.setVisibility(View.GONE);
-            _imageView.setVisibility(View.GONE);
+            _imageView.setVisibility(View.INVISIBLE);
+            _noPreviewLayout.setVisibility(View.VISIBLE);
+            _iconTextView.setText(getIcon(_extension));
         } else {
             _progressBar.setVisibility(View.VISIBLE);
             _imageView.setVisibility(View.INVISIBLE);
+            _noPreviewLayout.setVisibility(View.GONE);
         }
 
         _descriptionEditText.setText(misc.isEmptyOrNull(_description) ? "" : _description);
