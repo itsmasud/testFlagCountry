d9ffd8e7df5daa3613e62d573673bb268038e081
SHOAIB AHMED <its.masud@yahoo.com>
2018-03-30 17:34:31 +0600

    MAR-1579
    fixed a major flaw in FindWebTransactionTask

--- projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/transaction/WebTransactionUtils.java
+++ projects/FieldNation/fieldnation/src/main/java/com/fieldnation/service/transaction/WebTransactionUtils.java
@@ -20,46 +20,39 @@ public class WebTransactionUtils {
     // WebTransaction Key
     public static final String WEB_TRANS_KEY_PREFIX_CLOSING_NOTES = "%/updateClosingNotesByWorkOrder/api/rest/v2/workorders/";
 
-    private static WebTransactionUtils _webTransUtils;
-
-    // Data
-    private Listener _listener;
-
     // TODO more key type will be added
     public enum KeyType {
         CLOSING_NOTES
     }
 
-    public static WebTransactionUtils getOb() {
-        if (_webTransUtils == null) _webTransUtils =  new WebTransactionUtils();
-        return _webTransUtils;
-    }
-
-    // TODO more cases will be added while implementing more offline feature
-    private static String getWebTransKeyByType(KeyType keyType, int workOrderId){
-        switch (keyType){
-            case CLOSING_NOTES: return WEB_TRANS_KEY_PREFIX_CLOSING_NOTES + workOrderId + "%";
+    // TODO more cases will be added while implementing more offline features
+    private static String getWebTransKeyByType(KeyType keyType, int workOrderId) {
+        switch (keyType) {
+            case CLOSING_NOTES:
+                return WEB_TRANS_KEY_PREFIX_CLOSING_NOTES + workOrderId + "%";
         }
         return null;
     }
 
-    // TODO more cases will be added while implementing more offline feature
-    private static String getParamKeyByType(KeyType keyType){
-        switch (keyType){
-            case CLOSING_NOTES: return PARAM_CLOSING_NOTES_KEY;
+    // TODO more cases will be added while implementing more offline features
+    private static String getParamKeyByType(KeyType keyType) {
+        switch (keyType) {
+            case CLOSING_NOTES:
+                return PARAM_CLOSING_NOTES_KEY;
         }
         return null;
     }
 
     public static void setData(Listener listener, KeyType keyType, int workOrderId) {
-        getOb()._listener = listener;
-        new FindWebTransactionTask().executeEx(getWebTransKeyByType(keyType, workOrderId), getParamKeyByType(keyType));
+        new FindWebTransactionTask(listener).executeEx(getWebTransKeyByType(keyType, workOrderId), getParamKeyByType(keyType));
     }
 
 
     private static class FindWebTransactionTask extends AsyncTaskEx<Object, Object, Object> {
+        private Listener listener;
 
-        public FindWebTransactionTask() {
+        public FindWebTransactionTask(Listener listener) {
+            this.listener = listener;
         }
 
         @Override
@@ -86,7 +79,7 @@ public class WebTransactionUtils {
 
         @Override
         protected void onPostExecute(Object o) {
-            getOb()._listener.onFoundWebTransaction((WebTransaction) o);
+            this.listener.onFoundWebTransaction((WebTransaction) o);
             super.onPostExecute(o);
         }
     }
